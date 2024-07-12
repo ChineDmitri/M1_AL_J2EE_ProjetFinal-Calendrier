cfe6afbe321673c61ba5af1ae2ca788c79948898
GALLIC-A <axel.gallic@orange.fr>
2024-07-12 09:11:23 +0200

    - Renommage des contrôleurs - Ajout d'un test de la redirection vers la page de login si l'utilisateur n'est pas connecté

--- src/test/java/com/esgi/calendar/controller/rest/WeeklyCalendarRestControllerIntegrationTI.java
+++ src/test/java/com/esgi/calendar/controller/rest/WeeklyCalendarRestControllerIntegrationTI.java
@@ -1,6 +1,5 @@
 package com.esgi.calendar.controller.rest;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,19 +8,18 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@WithMockUser(username = "testuser", roles = "USER")
-public class WeeklyCalendarRestControllerIntegrationTest {
+public class WeeklyCalendarRestControllerIntegrationTI {
 
     @Autowired
     private MockMvc mockMvc;
 
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     void testGetGifForDaySuccess() throws
                                    Exception {
         int validDayId = 1;
@@ -32,6 +30,7 @@ public class WeeklyCalendarRestControllerIntegrationTest {
     }
 
     @Test
+    @WithMockUser(username = "testuser", roles = "USER")
     void testGetGifForDayTechnicalError() throws
                                           Exception {
         int invalidDayId = -1;
@@ -39,6 +38,15 @@ public class WeeklyCalendarRestControllerIntegrationTest {
         mockMvc.perform(get("/api/day/" + invalidDayId))
                .andExpect(status().isBadRequest())
                .andExpect(jsonPath("$.status").value("BAD_REQUEST"));
+    }
+
+    @Test
+    void testGetGifForDay_shouldRedirectOnLoginPage() throws Exception {
+        mockMvc.perform(get("/api/day/0"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"))
+                //.andExpect(jsonPath("$.status").value(""))
+        ;
     }
 
 }
